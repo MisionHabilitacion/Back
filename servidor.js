0ad104a71fe1e8492c26b0f8974b003cf9a97eeb
elimay415 <elimay415@yahoo.com>
2021-10-23 15:15:41 -0500

    creación de rutas y api para comunicación del api y rutas de creacion de va y obtencion de venta para comunicación del front y el back

--- servidor.js
+++ servidor.js
@@ -2,50 +2,96 @@
 //const express = require ('express');
 //pero al poner (type= "module";) en el package.json podemos realizar la import como se realiza normalmente.
 import Express from 'express';
+import { MongoClient } from 'mongodb';
 
-const app = Express()
+//crear string de conexión
+const stringConexion = 'mongodb+srv://elimay:elimay415@pruebamisiontic.jdckl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
-app.listen(5000, ()=>{
-    console.log('escuchando puerto 5000');
+//creación de instancia de mongoClient
+const client = new MongoClient(stringConexion, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 });
 
-app.get('vtas', (req,res) => {
+let conexion;
 
-    const vtas =[
-        {
-          _id: "v456",
-          fecha: "4/2/21" ,
-          estado: "Activo",
-          vendedor: "Camilo Parra" , 
-          idVendedor: "3276545",
-          cliente: "Mirian medina" ,
-          tipoIdCliente: 'cedula',
-          idCliente: "cv002" ,
-          producto: "Camisa Dama",
-          idPcto: "cf342" , 
-          cantidad: 4 ,
-          precio: 3000,
-          subTotal: 12000,
-          total : 400000,
-        } ,
-        {
-          _id: "v456",
-          fecha: "4/2/21" ,
-          estado: "Activo",
-          vendedor: "Camilo Parra" , 
-          idVendedor: "3276545",
-          cliente: "Mirian medina" ,
-          tipoIdCliente: 'cedula',
-          idCliente: "cv002" ,
-          producto: "Camisa Dama",
-          idPcto: "cf342" , 
-          cantidad: 4 ,
-          precio: 3000,
-          subTotal: 12000,
-          total : 400000,
-        } 
-      ];
-      res.send(vtas)
+const app = Express();
+app.use(Express.json());
+/*
+app.delete('vtas/eliminar',(req, res) => {
+   //revisar
+  res.send(vtas);
+})
 
+app.patch ('vtas/actualizar', (req, res) => {
+  //revisar
+  res.send(vtas);
+});*/
 
+//Creación de vta
+app.post('/vtas/nuevo', (req,res) => {
+  console.log(req);
+  const datosVta = req.body;
+  console.log('llaves', Object.keys(datosVta));
+  try{
+    if(
+    Object.keys(datosVta).includes("idVta") &&
+    Object.keys(datosVta).includes("fecha") &&
+    Object.keys(datosVta).includes("estado") && 
+    Object.keys(datosVta).includes("vendedor") &&
+    Object.keys(datosVta).includes("cliente") &&
+    Object.keys(datosVta).includes("tipoIdCliente") &&
+    Object.keys(datosVta).includes("idCliente") &&
+    Object.keys(datosVta).includes("producto") &&
+    Object.keys(datosVta).includes( "idPcto") &&
+    Object.keys(datosVta).includes("cantidad") &&
+    Object.keys(datosVta).includes("subTotal") &&
+    Object.keys(datosVta).includes("total") 
+    ){
+      conexion.collection('vtas').insertOne(datosVta, (err,result) => {
+        if(err){
+          console.error(err);
+          res.sendStatus(500);
+        }else{
+          console.log(result);
+          res.sendStatus(200);
+        }
+      });
+    }else{
+      res.sendStatus(500);
+    }
+  }catch{
+    res.sendStatus(500);
+  }
+  /*console.log('vta registrada', req.body);
+  console.log('esta es una solicitud post a vtas/nuevo')
+  res.send('vta registrada');*/
 });
+
+//solicitud de info de vtas a base de datos
+app.get('/vtas', (req, res) => {
+  console.log('alguien hizo get en la ruta vtas');
+  conexion.collection('vtas').find({}).limit(50).toArray((err,result)=>{
+    if(err){
+      res.status(500).send('Error en consulta de ventas');
+    }else{
+      res.json(result);
+    }
+  });
+});
+
+const main = () => {
+  client.connect((err, db)=>{
+    if(err){
+      console.error('Error conectando a la base de datos');
+    }
+    conexion = db.db('rookies');
+    console.log('conexion exitosa');
+    return app.listen(5000, ()=>{
+      console.log('escuchando puerto 5000');
+    });
+  });
+  
+};
+
+main();
